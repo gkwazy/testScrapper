41b81aaf6dec172d24dd3236872affd0a923f50b
gkwazy <no-replay>
2018-08-28 17:41:37 -0600

    scraping works

--- public/app.js
+++ public/app.js
@@ -41,7 +41,20 @@ $(document).on("click", "p", function () {
             }
         });
 });
-
+$(document).on("click", ".buttonScrape", function () {
+    // Empty the notes from the note section
+    $("#notes").empty();
+    // Now make an ajax call for the Article
+    $.ajax({
+        method: "GET",
+        url: "/scrape"
+    })
+        // With that done, add the note information to the page
+        .then(function (data) {
+            console.log("functions ran");
+            location.reload();
+        });
+});
 // When you click the savenote button
 $(document).on("click", "#savenote", function () {
     // Grab the id associated with the article from the submit button
